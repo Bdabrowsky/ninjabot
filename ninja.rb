b3ca66da00021677630414d8e8d18e93c92c2a0e
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-07-18 20:34:47 +0200

    0.1
    adding weather for project

--- ninja.rb
+++ ninja.rb
@@ -1,14 +1,26 @@
 
 
+require 'open_weather'
 require 'telegram/bot'
-Telegram::Bot::Client.run('658761373:AAHd3ga9pafA_eWHA16hA_DrcqiZ62BjLKc') do |bot|
+
+
+
+options_Open_Weather = { units: "fallows", APPID: "e27e534d44d1af80ca4fb157362d523a
+" }
+options_Bot = '658761373:AAHd3ga9pafA_eWHA16hA_DrcqiZ62BjLKc'
+
+
+
+
+
+Telegram::Bot::Client.run(options_Bot) do |bot|
     bot.listen do |message|
       case message.text
         when 'Hi' 
           bot.api.send_message(chat_id: message.chat.id, text: "Hello!")
         
-        when 'Weather?'
-            bot.api.send_message(chat_id: message.chat.id, text: "Sorry, I don't know.")
+        when '/Weather'
+            bot.api.send_message(chat_id: message.chat.id, text: "Your weather: #{OpenWeather::Current.city("Cochin, IN", options_Open_Weather)} ")           
         end
       end
     end
