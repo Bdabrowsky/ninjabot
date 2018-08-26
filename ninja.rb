20ce90b312aabe1d216e22da662114d145436d4a
Bdabrowswki <bartosz.dabrowski.org@gmail.com>
2018-08-26 20:02:21 +0200

    1.1.1.1

--- ninja.rb
+++ ninja.rb
@@ -5,23 +5,42 @@ require 'telegram/bot'
 
 
 
-options_Open_Weather = { units: "fallows", APPID: "e27e534d44d1af80ca4fb157362d523a
-" }
-options_Bot = '658761373:AAHd3ga9pafA_eWHA16hA_DrcqiZ62BjLKc'
-
-
-
+options_OpenWeather = { units: "metric", APPID: "access key" }
+options_Bot = 'access key'
 
 
 Telegram::Bot::Client.run(options_Bot) do |bot|
+  
+  
     bot.listen do |message|
+      kb = [
+            Telegram::Bot::Types::KeyboardButton.new(text: 'Show me your location', request_location: true)           
+           ]
+           markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+          
+           pp weather = OpenWeather::Current.geocode(53.11, 23.36, options_OpenWeather)
       case message.text
-        when 'Hi' 
-          bot.api.send_message(chat_id: message.chat.id, text: "Hello!")
-        
-        when '/Weather'
-            bot.api.send_message(chat_id: message.chat.id, text: "Your weather: #{OpenWeather::Current.city("Cochin, IN", options_Open_Weather)} ")           
+        when '/start'
+          bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}!")
+        when '/end'
+          bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}!")     
+        when '/help'
+          bot.api.send_message(chat_id: message.chat.id, text: "Available commands:
+          /start
+          /end
+          /help
+          /hi
+          /weather
+          /test")
+        when '/hi' 
+          bot.api.send_message(chat_id: message.chat.id, text: 'Hey!', reply_markup: markup)         
+         when '/weather'
+          bot.api.send_message(chat_id: message.chat.id, text: "Weather in your location: #{weather["main"]["temp"]} celcius, #{weather["main"]["pressure"]} hPa. Humidity is #{weather["main"]["humidity"]}% ")                    
+        when '/test'
+          bot.api.send_message(chat_id: message.chat.id, text:"I'm testing command")
+        else
+          bot.api.send_message(chat_id: message.chat.id, text: "I don't understand you 😕")
         end
       end
     end
-  
+    
